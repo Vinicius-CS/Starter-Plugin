b560eec9ffd14bf5b46f21b7ab58ba8db7eb6b16
Vinícius <48515934+Vinicius-CS@users.noreply.github.com>
2021-08-01 11:05:44 -0300

    Starter Plugin

---
+++ Starter Plugin/Starter.cs
@@ -0,0 +1,33 @@
+﻿using PluginDM;
+
+namespace PluginBuilderDM
+{
+    public class Starter:PluginIDM
+    {
+        public string Plugin_Name()
+        {
+            return "Plugin Name";
+        }
+
+        public string Plugin_Description()
+        {
+            return "Plugin Description";
+        }
+
+        public string Plugin_Version()
+        {
+            return "1.0.0.0";
+        }
+
+        /*public void MenuAdder(System.Windows.Forms.ToolStripItem ts)
+        {
+            ts.Click += ts_Click;
+        }
+
+        void ts_Click(object sender, EventArgs e)
+        {
+            Library_Dashboard frm = new Library_Dashboard();
+            frm.ShowDialog();
+        }*/
+    }
+}
---
+++ Starter Plugin/Starter_Plugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Starter_Plugin
+{
+    public partial class Starter_Plugin : Form
+    {
+        public Starter_Plugin()
+        {
+            InitializeComponent();
+        }
+    }
+}
